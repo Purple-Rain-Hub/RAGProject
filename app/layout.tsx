b92f6648e027270d12a052b0cf42d6fad79c15ac
Manuel Palmieri <palmierimanuel2002@gmail.com>
2025-09-01 10:54:44 +0200

    installato tailwind

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { ReactNode } from 'react';
+import './globals.css';
 
 export const runtime = 'nodejs';
 export default function RootLayout({ children }: { children: ReactNode }) {
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,11 @@
+"use client";
+
 export default function Page() {
-  return <main style={{padding:20,fontFamily:'ui-sans-serif, system-ui'}}>Hello Next.js</main>
+  
+  return (
+    <main className="p-8 bg-blue-50 min-h-screen">
+      <h1 className="text-3xl font-bold text-blue-900 mb-4">Hello Next.js with Tailwind!</h1>
+      <p className="text-gray-700">Se vedi questo testo stilizzato, Tailwind funziona correttamente.</p>
+    </main>
+  )
 }
