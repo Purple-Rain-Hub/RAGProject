8479a676e4e17c14d44b094365a63e7f4ac9300b
Manuel Palmieri <palmierimanuel2002@gmail.com>
2025-09-01 10:31:58 +0200

    installato next.js

---
+++ app/layout.tsx
@@ -0,0 +1,12 @@
+import type { ReactNode } from 'react';
+
+export const runtime = 'nodejs';
+export default function RootLayout({ children }: { children: ReactNode }) {
+  return (
+    <html lang="en">
+      <body>
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,3 @@
+export default function Page() {
+  return <main style={{padding:20,fontFamily:'ui-sans-serif, system-ui'}}>Hello Next.js</main>
+}
